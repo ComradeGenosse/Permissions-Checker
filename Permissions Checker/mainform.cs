0df6e1a86be31d2e4c1f601b746f8f9943263ed0
ComradeGenosse <chris.scoggins@configura.com>
2023-04-30 13:59:30 -0400

    V3

--- Permissions Checker/mainform.cs
+++ Permissions Checker/mainform.cs
@@ -5,6 +5,8 @@ using System.Windows.Forms;
 using System.Security.AccessControl;
 using Newtonsoft.Json.Linq;
 using System.Net;
+using System.Net.Sockets;
+using System.Net.Http;
 
 namespace PermissionsChecker
 {
@@ -14,79 +16,8 @@ namespace PermissionsChecker
 
         private Button btnAnalyze;
         private Button btnBrowse;
-        private RichTextBox txtStatusReport; // Updated
-        private RichTextBox txtUserTesting; // Updated
-
-        public MainForm()
-        {
-            InitializeComponent();
-        }
-
-        private void InitializeComponent()
-        {
-            this.btnAnalyze = new System.Windows.Forms.Button();
-            this.btnBrowse = new System.Windows.Forms.Button();
-            this.txtStatusReport = new System.Windows.Forms.RichTextBox();
-            this.txtUserTesting = new System.Windows.Forms.RichTextBox();
-            this.SuspendLayout();
-            // 
-            // btnAnalyze
-            // 
-            this.btnAnalyze.Location = new System.Drawing.Point(750, 234);
-            this.btnAnalyze.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnAnalyze.Name = "btnAnalyze";
-            this.btnAnalyze.Size = new System.Drawing.Size(112, 35);
-            this.btnAnalyze.TabIndex = 0;
-            this.btnAnalyze.Text = "Analyze";
-            this.btnAnalyze.UseVisualStyleBackColor = true;
-            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
-            // 
-            // btnBrowse
-            // 
-            this.btnBrowse.Location = new System.Drawing.Point(39, 14);
-            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.btnBrowse.Name = "btnBrowse";
-            this.btnBrowse.Size = new System.Drawing.Size(112, 35);
-            this.btnBrowse.TabIndex = 1;
-            this.btnBrowse.Text = "Browse";
-            this.btnBrowse.UseVisualStyleBackColor = true;
-            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
-            // 
-            // txtStatusReport
-            // 
-            this.txtStatusReport.Location = new System.Drawing.Point(357, 309);
-            this.txtStatusReport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtStatusReport.Name = "txtStatusReport";
-            this.txtStatusReport.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.txtStatusReport.Size = new System.Drawing.Size(818, 369);
-            this.txtStatusReport.TabIndex = 2;
-            this.txtStatusReport.Text = "";
-            // 
-            // txtUserTesting
-            // 
-            this.txtUserTesting.Location = new System.Drawing.Point(24, 59);
-            this.txtUserTesting.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.txtUserTesting.Name = "txtUserTesting";
-            this.txtUserTesting.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.txtUserTesting.Size = new System.Drawing.Size(575, 51);
-            this.txtUserTesting.TabIndex = 3;
-            this.txtUserTesting.Text = "";
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
-            this.Controls.Add(this.txtUserTesting);
-            this.Controls.Add(this.txtStatusReport);
-            this.Controls.Add(this.btnBrowse);
-            this.Controls.Add(this.btnAnalyze);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Name = "MainForm";
-            this.Text = "Permissions Checker";
-            this.ResumeLayout(false);
-
-        }
+        private RichTextBox txtStatusReport;
+        private RichTextBox txtUserTesting;
 
         private string[] folders = new string[]
         {
@@ -101,23 +32,122 @@ namespace PermissionsChecker
             @"%LOCALAPPDATA%\ProjectNotify",
             @"C:\ProgramData\ProjectMatrix"
         };
+
         private string[] folderNames = new string[]
         {
-        "Data Directory",
-        "Spec Folder",
-        "CIL for ACAD folder",
-        "Data Warehouse folder",
-        "CIL Apps folder",
-        "CIL Config folder",
-        "CIL User Files folder",
-        "CET Folder",
-        "Notify Folder",
-        "CIL Legacy data folder"
+            "Data Directory",
+            "Spec Folder",
+            "CIL for ACAD folder",
+            "Data Warehouse folder",
+            "CIL Apps folder",
+            "CIL Config folder",
+            "CIL User Files folder",
+            "CET Folder",
+            "Notify Folder",
+            "CIL Legacy data folder"
         };
 
-        private void MainForm_Load(object sender, EventArgs e)
+        private string[] webUrls = new string[]
         {
-            // Initialize UI elements
+            "http://www.projectmatrix.com",
+            "https://www.projectmatrix.com",
+            "https://login.projectmatrix.com",
+            "https://data.projectmatrix.com/",
+            "https://api.projectmatrix.com/datawarehouse/getEnterprises.php",
+            "https://api.projectmatrix.com/datawarehouse/getCatalogs.php"
+        };
+
+        private string[] webUrlNames = new string[]
+        {
+            "ProjectMatrix Website (HTTP)",
+            "ProjectMatrix Website (HTTPS)",
+            "ProjectMatrix Login",
+            "Data Download Location",
+            "API GetEnterprises",
+            "API GetCatalogs"
+        };
+
+        private int[] ports = new int[]
+        {
+            60294,
+            8730,
+            8731
+        };
+
+        private string[] portNames = new string[]
+        {
+            "License Server/Login Check (60294)",
+            "ProjectNotify Server (8730)",
+            "ProjectNotify Client (8731)"
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.BackgroundImage = Image.FromFile(@"C:\CETDEV\PMX Tool Image.png");
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtStatusReport = new System.Windows.Forms.RichTextBox();
+            this.txtUserTesting = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(12, 12);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyze.TabIndex = 0;
+            this.btnAnalyze.Text = "Analyze";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 41);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtStatusReport
+            // 
+            this.txtStatusReport.Location = new System.Drawing.Point(93, 12);
+            this.txtStatusReport.Multiline = true;
+            this.txtStatusReport.Name = "txtStatusReport";
+            this.txtStatusReport.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtStatusReport.Size = new System.Drawing.Size(400, 150);
+            this.txtStatusReport.TabIndex = 2;
+            // 
+            // txtUserTesting
+            // 
+            this.txtUserTesting.Location = new System.Drawing.Point(12, 70);
+            this.txtUserTesting.Multiline = true;
+            this.txtUserTesting.Name = "txtUserTesting";
+            this.txtUserTesting.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtUserTesting.Size = new System.Drawing.Size(400, 150);
+            this.txtUserTesting.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtUserTesting);
+            this.Controls.Add(this.txtStatusReport);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnAnalyze);
+            this.Name = "MainForm";
+            this.Text = "Permissions Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         private void btnAnalyze_Click(object sender, EventArgs e)
@@ -138,7 +168,7 @@ namespace PermissionsChecker
             }
         }
 
-        private void AnalyzePermissions()
+        private async void AnalyzePermissions()
         {
             txtStatusReport.Clear();
 
@@ -149,85 +179,119 @@ namespace PermissionsChecker
 
                 if (Directory.Exists(folderPath))
                 {
-                    AppendColoredText(txtStatusReport, $"{folderName} - ", Color.Black);
-                    AppendColoredText(txtStatusReport, $"{CheckPermissions(folderPath)}\r\n", CheckPermissions(folderPath).Contains("PASS") ? Color.Green : Color.Red);
+                    AppendTextWithColor(txtStatusReport, $"{folderName} - ", Color.Black);
+                    CheckPermissions(folderPath);
                 }
             }
 
-            bool awsServerStatus = PingMySQL();
-            AppendColoredText(txtStatusReport, $"AWS Server: ", Color.Black);
-            AppendColoredText(txtStatusReport, $"{(awsServerStatus ? "PASS" : "FAIL")}", awsServerStatus ? Color.Green : Color.Red);
-        }
-
-        private bool PingMySQL(bool isRemote = false)
-        {
-            try
+            for (int i = 0; i < webUrls.Length; i++)
             {
-                string json = new WebClient().DownloadString("https://api.projectmatrix.com/datawarehouse/pingDatabase.php?key=" + ApiKey);
-                return JObject.Parse(json)["ping_successful"].ToObject<bool>();
+                string url = webUrls[i];
+                string urlName = webUrlNames[i];
+
+                AppendTextWithColor(txtStatusReport, $"{urlName} - ", Color.Black);
+                await CheckWebUrl(url);
             }
-            catch (Exception ex)
+
+            for (int i = 0; i < ports.Length; i++)
             {
-                return false;
+                int port = ports[i];
+                string portName = portNames[i];
+
+                AppendTextWithColor(txtStatusReport, $"{portName} - ", Color.Black);
+                CheckPort(port);
             }
-        }
 
-        private void AnalyzeCustomFolder(string folderPath)
-        {
-            txtUserTesting.Clear();
-            AppendColoredText(txtUserTesting, $"{Path.GetFileName(folderPath)} - ", Color.Black);
-            AppendColoredText(txtUserTesting, $"{CheckPermissions(folderPath)}\r\n", CheckPermissions(folderPath).Contains("PASS") ? Color.Green : Color.Red);
-        }
-
-        private string CheckPermissions(string folderPath)
-        {
-            try
+            private async void AnalyzeCustomFolder(string folderPath)
             {
-                DirectoryInfo di = new DirectoryInfo(folderPath);
-                DirectorySecurity ds = di.GetAccessControl();
+                txtUserTesting.Clear();
+                AppendTextWithColor(txtUserTesting, $"{Path.GetFileName(folderPath)} - ", Color.Black);
+                CheckPermissions(folderPath);
+            }
 
-                AuthorizationRuleCollection rules = ds.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
-
-                bool canRead = false;
-                bool canWrite = false;
-                bool canExecute = false;
-
-                foreach (FileSystemAccessRule rule in rules)
+            private void CheckPermissions(string folderPath)
+            {
+                try
                 {
-                    if ((rule.FileSystemRights & FileSystemRights.Read) == FileSystemRights.Read)
+                    DirectoryInfo di = new DirectoryInfo(folderPath);
+                    DirectorySecurity dSecurity = di.GetAccessControl();
+
+                    AuthorizationRuleCollection acl = dSecurity.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount));
+                    bool isWritable = false;
+
+                    foreach (FileSystemAccessRule rule in acl)
                     {
-                        canRead = true;
+                        if ((FileSystemRights.Write & rule.FileSystemRights) == FileSystemRights.Write)
+                        {
+                            isWritable = true;
+                            break;
+                        }
                     }
-                    if ((rule.FileSystemRights & FileSystemRights.Write) == FileSystemRights.Write)
+
+                    if (isWritable)
                     {
-                        canWrite = true;
+                        AppendTextWithColor(txtStatusReport, "PASS\n", Color.Green);
                     }
-                    if ((rule.FileSystemRights & FileSystemRights.ExecuteFile) == FileSystemRights.ExecuteFile)
+                    else
                     {
-                        canExecute = true;
+                        AppendTextWithColor(txtStatusReport, "FAIL\n", Color.Red);
                     }
                 }
-
-                string result = $"Read: {(canRead ? "PASS" : "FAIL")}, Write: {(canWrite ? "PASS" : "FAIL")}, Execute: {(canExecute ? "PASS" : "FAIL")}";
-
-                return result;
+                catch (Exception ex)
+                {
+                    AppendTextWithColor(txtStatusReport, "FAIL\n", Color.Red);
+                }
             }
-            catch (Exception ex)
+
+            private async System.Threading.Tasks.Task CheckWebUrl(string url)
             {
-                return "FAIL";
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        HttpResponseMessage response = await client.GetAsync(url);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            AppendTextWithColor(txtStatusReport, "PASS\n", Color.Green);
+                        }
+                        else
+                        {
+                            AppendTextWithColor(txtStatusReport, "FAIL\n", Color.Red);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AppendTextWithColor(txtStatusReport, "FAIL\n", Color.Red);
+                }
             }
-        }
 
-        private void AppendColoredText(RichTextBox rtb, string text, Color color)
-        {
-            rtb.SelectionStart = rtb.TextLength;
-            rtb.SelectionLength = 0;
+            private void CheckPort(int port)
+            {
+                try
+                {
+                    using (TcpClient tcpClient = new TcpClient())
+                    {
+                        tcpClient.Connect("127.0.0.1", port);
+                        AppendTextWithColor(txtStatusReport, "PASS\n", Color.Green);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AppendTextWithColor(txtStatusReport, "FAIL\n", Color.Red);
+                }
+            }
 
-            rtb.SelectionColor = color;
-            rtb.AppendText(text);
-            rtb.SelectionColor = rtb.ForeColor;
-        }
-    }
-}
+            private void AppendTextWithColor(RichTextBox box, string text, Color color)
+            {
+                int start = box.TextLength;
+                box.AppendText(text);
+                int end = box.TextLength;
 
-
+                box.Select(start, end - start);
+                {
+                    box.SelectionColor = color;
+                }
+                box.SelectionLength = 0;
+            }
